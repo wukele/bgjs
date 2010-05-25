52c210ea516617340f92d52e349bf055a3a26118
javeejy@gmail.com <javeejy@gmail.com@ae75211e-6bd3-11de-ae5f-3f045535ef68>
2010-05-25 11:07:20 +0000

    史上最齐全的文档已经发布!

--- trunk/cicy/doc/jsdoc/output/tree.js
+++ trunk/cicy/doc/jsdoc/output/tree.js
@@ -2,15 +2,127 @@
 		Docs.classData ={"id":"apidocs","iconCls":"icon-docs","text":"API Documentation","singleClickExpand":true,"children":[
                 {"id":"pkg-CC","text":"CC","iconCls":"icon-pkg","cls":"package","singleClickExpand":true, children:[
                 {"id":"pkg-CC.layout","text":"layout","iconCls":"icon-pkg","cls":"package","singleClickExpand":true, children:[
+                {"href":"output/CC.layout.BorderLayout.html","text":"BorderLayout","id":"CC.layout.BorderLayout","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.layout.CardLayout.html","text":"CardLayout","id":"CC.layout.CardLayout","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
                 {"href":"output/CC.layout.Layout.html","text":"Layout","id":"CC.layout.Layout","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.layout.QQLayout.html","text":"QQLayout","id":"CC.layout.QQLayout","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.layout.RowLayout.html","text":"RowLayout","id":"CC.layout.RowLayout","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.layout.TableLayout.html","text":"TableLayout","id":"CC.layout.TableLayout","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.layout.TablizeLayout.html","text":"TablizeLayout","id":"CC.layout.TablizeLayout","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				]}
 				,
                 {"id":"pkg-CC.ui","text":"ui","iconCls":"icon-pkg","cls":"package","singleClickExpand":true, children:[
+                {"id":"pkg-CC.ui.form","text":"form","iconCls":"icon-pkg","cls":"package","singleClickExpand":true, children:[
+                {"href":"output/CC.ui.form.Checkbox.html","text":"Checkbox","id":"CC.ui.form.Checkbox","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.form.Combox.html","text":"Combox","id":"CC.ui.form.Combox","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.form.DatepickerField.html","text":"DatepickerField","id":"CC.ui.form.DatepickerField","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.form.FormElement.html","text":"FormElement","id":"CC.ui.form.FormElement","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.form.Progressbar.html","text":"Progressbar","id":"CC.ui.form.Progressbar","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.form.Radio.html","text":"Radio","id":"CC.ui.form.Radio","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.form.Select.html","text":"Select","id":"CC.ui.form.Select","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.form.StoreProvider.html","text":"StoreProvider","id":"CC.ui.form.StoreProvider","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.form.Text.html","text":"Text","id":"CC.ui.form.Text","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.form.Textarea.html","text":"Textarea","id":"CC.ui.form.Textarea","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				]}
+				,
+                {"id":"pkg-CC.ui.grid","text":"grid","iconCls":"icon-pkg","cls":"package","singleClickExpand":true, children:[
+                {"id":"pkg-CC.ui.grid.plugins","text":"plugins","iconCls":"icon-pkg","cls":"package","singleClickExpand":true, children:[
+                {"href":"output/CC.ui.grid.plugins.Editation.html","text":"Editation","id":"CC.ui.grid.plugins.Editation","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.grid.plugins.Pagenation.html","text":"Pagenation","id":"CC.ui.grid.plugins.Pagenation","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.grid.plugins.Toolbar.html","text":"Toolbar","id":"CC.ui.grid.plugins.Toolbar","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				]}
+				,
+                {"href":"output/CC.ui.grid.Cell.html","text":"Cell","id":"CC.ui.grid.Cell","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.grid.Column.html","text":"Column","id":"CC.ui.grid.Column","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.grid.Content.html","text":"Content","id":"CC.ui.grid.Content","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.grid.ContentStoreProvider.html","text":"ContentStoreProvider","id":"CC.ui.grid.ContentStoreProvider","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.grid.ContentValidation.html","text":"ContentValidation","id":"CC.ui.grid.ContentValidation","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.grid.Header.html","text":"Header","id":"CC.ui.grid.Header","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.grid.Row.html","text":"Row","id":"CC.ui.grid.Row","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				]}
+				,
+                {"href":"output/CC.ui.Bigbar.html","text":"Bigbar","id":"CC.ui.Bigbar","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Button.html","text":"Button","id":"CC.ui.Button","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
                 {"href":"output/CC.ui.ContainerBase.html","text":"ContainerBase","id":"CC.ui.ContainerBase","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Datepicker.html","text":"Datepicker","id":"CC.ui.Datepicker","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Dialog.html","text":"Dialog","id":"CC.ui.Dialog","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.DropButton.html","text":"DropButton","id":"CC.ui.DropButton","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.FloatTip.html","text":"FloatTip","id":"CC.ui.FloatTip","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Foldable.html","text":"Foldable","id":"CC.ui.Foldable","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Folder.html","text":"Folder","id":"CC.ui.Folder","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Grid.html","text":"Grid","id":"CC.ui.Grid","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.IFramePanel.html","text":"IFramePanel","id":"CC.ui.IFramePanel","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				,
                 {"href":"output/CC.ui.Item.html","text":"Item","id":"CC.ui.Item","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				,
+                {"href":"output/CC.ui.Loading.html","text":"Loading","id":"CC.ui.Loading","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Mask.html","text":"Mask","id":"CC.ui.Mask","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Menu.html","text":"Menu","id":"CC.ui.Menu","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.MenuItem.html","text":"MenuItem","id":"CC.ui.MenuItem","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Menubar.html","text":"Menubar","id":"CC.ui.Menubar","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
                 {"href":"output/CC.ui.Panel.html","text":"Panel","id":"CC.ui.Panel","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Resizer.html","text":"Resizer","id":"CC.ui.Resizer","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Shadow.html","text":"Shadow","id":"CC.ui.Shadow","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Smallbar.html","text":"Smallbar","id":"CC.ui.Smallbar","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Tab.html","text":"Tab","id":"CC.ui.Tab","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.TabItem.html","text":"TabItem","id":"CC.ui.TabItem","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.TabItemLayout.html","text":"TabItemLayout","id":"CC.ui.TabItemLayout","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.TitlePanel.html","text":"TitlePanel","id":"CC.ui.TitlePanel","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Tree.html","text":"Tree","id":"CC.ui.Tree","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.TreeItem.html","text":"TreeItem","id":"CC.ui.TreeItem","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Viewport.html","text":"Viewport","id":"CC.ui.Viewport","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.Win.html","text":"Win","id":"CC.ui.Win","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.menu.html","text":"menu","id":"CC.ui.menu","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				]}
 				,
                 {"id":"pkg-CC.util","text":"util","iconCls":"icon-pkg","cls":"package","singleClickExpand":true, children:[
@@ -30,11 +142,15 @@
                 {"href":"output/CC.util.dd.Mgr.resizeHelper.html","text":"Mgr.resizeHelper","id":"CC.util.dd.Mgr.resizeHelper","isClass":true,"iconCls":"icon-static","cls":"cls","leaf":true}
 				]}
 				,
+                {"href":"output/CC.util.AsynchronizeQueue.html","text":"AsynchronizeQueue","id":"CC.util.AsynchronizeQueue","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
                 {"href":"output/CC.util.BrushFactory.html","text":"BrushFactory","id":"CC.util.BrushFactory","isClass":true,"iconCls":"icon-static","cls":"cls","leaf":true}
 				,
                 {"href":"output/CC.util.ConnectionProvider.html","text":"ConnectionProvider","id":"CC.util.ConnectionProvider","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				,
                 {"href":"output/CC.util.CssParser.html","text":"CssParser","id":"CC.util.CssParser","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.util.IFrameConnectionProvider.html","text":"IFrameConnectionProvider","id":"CC.util.IFrameConnectionProvider","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				,
                 {"href":"output/CC.util.ProviderBase.html","text":"ProviderBase","id":"CC.util.ProviderBase","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				,
@@ -42,9 +158,15 @@
 				,
                 {"href":"output/CC.util.SelectionProvider.html","text":"SelectionProvider","id":"CC.util.SelectionProvider","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				,
+                {"href":"output/CC.util.StoreProvider.html","text":"StoreProvider","id":"CC.util.StoreProvider","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
                 {"href":"output/CC.util.Tracker.html","text":"Tracker","id":"CC.util.Tracker","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				,
                 {"href":"output/CC.util.TypeConverter.html","text":"TypeConverter","id":"CC.util.TypeConverter","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.util.ValidationProvider.html","text":"ValidationProvider","id":"CC.util.ValidationProvider","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.util.Validators.html","text":"Validators","id":"CC.util.Validators","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				]}
 				,
                 {"href":"output/CC.Ajax.html","text":"Ajax","id":"CC.Ajax","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
@@ -59,7 +181,7 @@
 				,
                 {"href":"output/CC.Tpl.html","text":"Tpl","id":"CC.Tpl","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				,
-                {"href":"output/CC.Util.html","text":"Util","id":"CC.Util","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+                {"href":"output/CC.Util.html","text":"Util","id":"CC.Util","isClass":true,"iconCls":"icon-static","cls":"cls","leaf":true}
 				,
                 {"href":"output/CC.ui.html","text":"ui","id":"CC.ui","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				,
@@ -74,23 +196,137 @@
 				,
                 {"href":"output/String.html","text":"String","id":"String","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				,
+                {"href":"output/console.html","text":"console","id":"console","isClass":true,"iconCls":"icon-static","cls":"cls","leaf":true}
+				,
                 {"href":"output/CC.util.d2d.html","text":"d2d","id":"CC.util.d2d","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				,
                 {"href":"output/CC.util.dd.html","text":"dd","id":"CC.util.dd","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				,
+                {"href":"output/CC.ui.form.html","text":"form","id":"CC.ui.form","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
                 {"href":"output/global.html","text":"global","id":"global","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				,
+                {"href":"output/CC.ui.grid.html","text":"grid","id":"CC.ui.grid","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
                 {"href":"output/CC.layout.html","text":"layout","id":"CC.layout","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
+				,
+                {"href":"output/CC.ui.grid.plugin.html","text":"plugin","id":"CC.ui.grid.plugin","isClass":true,"iconCls":"icon-cls","cls":"cls","leaf":true}
 				]};
         Docs.icons = {
         
+			"CC.layout.BorderLayout":"icon-cls"
+			,
+			"CC.layout.CardLayout":"icon-cls"
+			,
 			"CC.layout.Layout":"icon-cls"
+			,
+			"CC.layout.QQLayout":"icon-cls"
+			,
+			"CC.layout.RowLayout":"icon-cls"
+			,
+			"CC.layout.TableLayout":"icon-cls"
+			,
+			"CC.layout.TablizeLayout":"icon-cls"
+			,
+			"CC.ui.form.Checkbox":"icon-cls"
+			,
+			"CC.ui.form.Combox":"icon-cls"
+			,
+			"CC.ui.form.DatepickerField":"icon-cls"
+			,
+			"CC.ui.form.FormElement":"icon-cls"
+			,
+			"CC.ui.form.Progressbar":"icon-cls"
+			,
+			"CC.ui.form.Radio":"icon-cls"
+			,
+			"CC.ui.form.Select":"icon-cls"
+			,
+			"CC.ui.form.StoreProvider":"icon-cls"
+			,
+			"CC.ui.form.Text":"icon-cls"
+			,
+			"CC.ui.form.Textarea":"icon-cls"
+			,
+			"CC.ui.grid.plugins.Editation":"icon-cls"
+			,
+			"CC.ui.grid.plugins.Pagenation":"icon-cls"
+			,
+			"CC.ui.grid.plugins.Toolbar":"icon-cls"
+			,
+			"CC.ui.grid.Cell":"icon-cls"
+			,
+			"CC.ui.grid.Column":"icon-cls"
+			,
+			"CC.ui.grid.Content":"icon-cls"
+			,
+			"CC.ui.grid.ContentStoreProvider":"icon-cls"
+			,
+			"CC.ui.grid.ContentValidation":"icon-cls"
+			,
+			"CC.ui.grid.Header":"icon-cls"
+			,
+			"CC.ui.grid.Row":"icon-cls"
+			,
+			"CC.ui.Bigbar":"icon-cls"
+			,
+			"CC.ui.Button":"icon-cls"
 			,
 			"CC.ui.ContainerBase":"icon-cls"
 			,
+			"CC.ui.Datepicker":"icon-cls"
+			,
+			"CC.ui.Dialog":"icon-cls"
+			,
+			"CC.ui.DropButton":"icon-cls"
+			,
+			"CC.ui.FloatTip":"icon-cls"
+			,
+			"CC.ui.Foldable":"icon-cls"
+			,
+			"CC.ui.Folder":"icon-cls"
+			,
+			"CC.ui.Grid":"icon-cls"
+			,
+			"CC.ui.IFramePanel":"icon-cls"
+			,
 			"CC.ui.Item":"icon-cls"
 			,
+			"CC.ui.Loading":"icon-cls"
+			,
+			"CC.ui.Mask":"icon-cls"
+			,
+			"CC.ui.Menu":"icon-cls"
+			,
+			"CC.ui.MenuItem":"icon-cls"
+			,
+			"CC.ui.Menubar":"icon-cls"
+			,
 			"CC.ui.Panel":"icon-cls"
+			,
+			"CC.ui.Resizer":"icon-cls"
+			,
+			"CC.ui.Shadow":"icon-cls"
+			,
+			"CC.ui.Smallbar":"icon-cls"
+			,
+			"CC.ui.Tab":"icon-cls"
+			,
+			"CC.ui.TabItem":"icon-cls"
+			,
+			"CC.ui.TabItemLayout":"icon-cls"
+			,
+			"CC.ui.TitlePanel":"icon-cls"
+			,
+			"CC.ui.Tree":"icon-cls"
+			,
+			"CC.ui.TreeItem":"icon-cls"
+			,
+			"CC.ui.Viewport":"icon-cls"
+			,
+			"CC.ui.Win":"icon-cls"
+			,
+			"CC.ui.menu":"icon-cls"
 			,
 			"CC.util.d2d.ComponentRect":"icon-cls"
 			,
@@ -104,11 +340,15 @@
 			,
 			"CC.util.dd.Mgr.resizeHelper":"icon-static"
 			,
+			"CC.util.AsynchronizeQueue":"icon-cls"
+			,
 			"CC.util.BrushFactory":"icon-static"
 			,
 			"CC.util.ConnectionProvider":"icon-cls"
 			,
 			"CC.util.CssParser":"icon-cls"
+			,
+			"CC.util.IFrameConnectionProvider":"icon-cls"
 			,
 			"CC.util.ProviderBase":"icon-cls"
 			,
@@ -116,9 +356,15 @@
 			,
 			"CC.util.SelectionProvider":"icon-cls"
 			,
+			"CC.util.StoreProvider":"icon-cls"
+			,
 			"CC.util.Tracker":"icon-cls"
 			,
 			"CC.util.TypeConverter":"icon-cls"
+			,
+			"CC.util.ValidationProvider":"icon-cls"
+			,
+			"CC.util.Validators":"icon-cls"
 			,
 			"CC.Ajax":"icon-cls"
 			,
@@ -132,7 +378,7 @@
 			,
 			"CC.Tpl":"icon-cls"
 			,
-			"CC.Util":"icon-cls"
+			"CC.Util":"icon-static"
 			,
 			"CC.ui":"icon-cls"
 			,
@@ -146,12 +392,20 @@
 			,
 			"String":"icon-cls"
 			,
+			"console":"icon-static"
+			,
 			"CC.util.d2d":"icon-cls"
 			,
 			"CC.util.dd":"icon-cls"
 			,
+			"CC.ui.form":"icon-cls"
+			,
 			"global":"icon-cls"
 			,
+			"CC.ui.grid":"icon-cls"
+			,
 			"CC.layout":"icon-cls"
+			,
+			"CC.ui.grid.plugin":"icon-cls"
 			};
     
